db0526d4959680305f4002cafa66d5804daf6405
DattaSAi4 <psaivinay123@gmail.com>
2022-01-16 18:58:44 +0530

    LOCKERAPPLICATION

---
+++ Main.java
@@ -0,0 +1,14 @@
+package LOCKER;
+import java.io.File;
+
+
+
+public class Main {
+	public static void main(String[] args)
+	{
+		//file class object 
+		file f1 = new file();
+		f1.selection();	
+	}
+
+}
